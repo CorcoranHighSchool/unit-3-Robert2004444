656103d0eb31e48f56adfef21efcf2706c90dcf0
Robert2004444 <98342793+Robert2004444@users.noreply.github.com>
2023-01-23 09:37:52 -0800

    Unit 3.1

--- 1 - Optionals/Lab - Optionals.playground/Pages/3. Exercise - Functions and Optionals.xcplaygroundpage/Contents.swift
+++ 1 - Optionals/Lab - Optionals.playground/Pages/3. Exercise - Functions and Optionals.xcplaygroundpage/Contents.swift
@@ -5,14 +5,34 @@
  */
 let userInputAge: String = "34e"
 
-
+func checkAge(age: String)-> Int?{
+    if let ageInt = Int (age) {
+        //this means age is an integer
+        //if they are 18 or older
+        if ageInt >= 18 {
+        print("Welcome!")
+        }else{
+            print("Sorry, but you are not old enough to use our app.")
+         }
+        return ageInt
+    }else{ //age is not an integer
+            print("Sorry, Something went wrong, please try to enter your age again?" )
+        return nil
+    }
+}
+checkAge(age: userInputAge) //Sorry, wrong input
+checkAge(age: "18")         //Welcome
+checkAge(age:"0")           //fail
 //:  Go back and update your function to return the age as an integer. Will your function always return a value? Make sure your return type accurately reflects this. Call the function and print the return value.
-
+print(checkAge(age: "34"))
+if let age = checkAge(age: "56"){
+    print(age)
+}
 
 //:  Imagine you are creating an app for making purchases. Write a function that will take in the name of an item for purchase as a `String` and will return the cost of that item as an optional `Double`. In the body of the function, check to see if the item is in stock by accessing it in the dictionary `stock`. If it is, return the price of the item by accessing it in the dictionary `prices`. If the item is out of stock, return `nil`. Call the function and pass in a `String` that exists in the dictionaries below. Print the return value.
 var prices = ["Chips": 2.99, "Donuts": 1.89, "Juice": 3.99, "Apple": 0.50, "Banana": 0.25, "Broccoli": 0.99]
 var stock = ["Chips": 4, "Donuts": 0, "Juice": 12, "Apple": 6, "Banana": 6, "Broccoli": 3]
-
+let price = prices["Chips"]
 
 /*:
 [Previous](@previous)  |  page 3 of 6  |  [Next: App Exercise - Food Functions](@next)
