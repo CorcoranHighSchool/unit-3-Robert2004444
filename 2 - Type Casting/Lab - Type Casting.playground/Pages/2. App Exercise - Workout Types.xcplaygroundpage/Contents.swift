9aca8cffc5f8413806507c5f9be3075e797e3a8e
Robert2004444 <98342793+Robert2004444@users.noreply.github.com>
2023-01-30 08:51:08 -0800

    3.2

--- 2 - Type Casting/Lab - Type Casting.playground/Pages/2. App Exercise - Workout Types.xcplaygroundpage/Contents.swift
+++ 2 - Type Casting/Lab - Type Casting.playground/Pages/2. App Exercise - Workout Types.xcplaygroundpage/Contents.swift
@@ -8,45 +8,80 @@
  Also provided is a `workouts` array that represents a log of past workouts. You'll use these classes and the array for the exercises below.
  */
 class Workout {
-    let time: Double
-    let distance: Double
 
-    init(time: Double, distance: Double) {
-        self.time = time
-        self.distance = distance
-    }
+let time: Double
+
+let distance: Double
+
+init(time: Double, distance: Double) {
+
+self.time = time
+
+self.distance = distance
+
+}
+
 }
 
 class Run: Workout {
-    let cadence: Double
 
-    init(cadence: Double, time: Double, distance: Double) {
-        self.cadence = cadence
-        super.init(time: time, distance: distance)
-    }
+let cadence: Double
+
+init(cadence: Double, time: Double, distance: Double) {
+
+self.cadence = cadence
+
+super.init(time: time, distance: distance)
+
+}
+
 }
 
 class Swim: Workout {
-    let stroke: String
 
-    init(stroke: String, time: Double, distance: Double) {
-        self.stroke = stroke
-        super.init(time: time, distance: distance)
-    }
+let stroke: String
+
+init(stroke: String, time: Double, distance: Double) {
+
+self.stroke = stroke
+
+super.init(time: time, distance: distance)
+
+}
+
 }
 
 var workouts: [Workout] = [
-    Run(cadence: 80, time: 1200, distance: 4000),
-    Swim(stroke: "Freestyle", time: 32.1, distance: 50),
-    Swim(stroke: "Butterfly", time: 36.8, distance: 50),
-    Swim(stroke: "Freestyle", time: 523.6, distance: 500),
+
+Run(cadence: 80, time: 1200, distance: 4000),
+
+Swim(stroke: "Freestyle", time: 32.1, distance: 50),
+
+Swim(stroke: "Butterfly", time: 36.8, distance: 50),
+
+Swim(stroke: "Freestyle", time: 523.6, distance: 500),
+
     Run(cadence: 90, time: 358.9, distance: 1600)
+
 ]
 //:  Write simple functions called `describeRun(runningWorkout:)` and `describeSwim(swimmingWorkout:)` that take a `Run` object and a `Swim` object, respectively. Neither should return values. Each function should print a description of the workout, including the run's cadence or the swim's stroke. Time is represented in seconds, distance is represented in meters, and cadence is represented in steps per minute.
- 
+func describeRun(runningWorkout: Run){
+    print("You ran \(runningWorkout.distance) metres in \(runningWorkout.time) seconds. Your candence was \(runningWorkout.cadence) steps per minute.")
+}
+func describeSwim(swimmingWorkout: Swim){
+    print("You swam \(swimmingWorkout.distance) metres in \(swimmingWorkout.time) seconds using a \(swimmingWorkout.stroke) stroke.")
+}
+    
 
 //:  Now loop through each workout in `workouts` and, using type casting, call either `describeRun(runningWorkout:)` or `describeSwim(swimmingWorkout:)` on each. Observe what is printed to the console.
- 
+for workout in workouts {
+    if let run = workout as? Run{
+        describeRun(runningWorkout: run)
+    }else if let swim = workout as? Swim{
+        describeSwim(swimmingWorkout: swim)
+       }
+    }
+
 
 /*:
  _Copyright © 2021 Apple Inc._
