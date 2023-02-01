682dff4d9379203ca2149fa0a6f2f4eb10a85a27
Robert2004444 <98342793+Robert2004444@users.noreply.github.com>
2023-02-01 10:02:35 -0800

    3.3

--- 2 - Type Casting/Lab - Type Casting.playground/Pages/2. App Exercise - Workout Types.xcplaygroundpage/Contents.swift
+++ 2 - Type Casting/Lab - Type Casting.playground/Pages/2. App Exercise - Workout Types.xcplaygroundpage/Contents.swift
@@ -66,10 +66,10 @@ Swim(stroke: "Freestyle", time: 523.6, distance: 500),
 ]
 //:  Write simple functions called `describeRun(runningWorkout:)` and `describeSwim(swimmingWorkout:)` that take a `Run` object and a `Swim` object, respectively. Neither should return values. Each function should print a description of the workout, including the run's cadence or the swim's stroke. Time is represented in seconds, distance is represented in meters, and cadence is represented in steps per minute.
 func describeRun(runningWorkout: Run){
-    print("You ran \(runningWorkout.distance) metres in \(runningWorkout.time) seconds. Your candence was \(runningWorkout.cadence) steps per minute.")
+    print("You ran \(runningWorkout.distance) meters in \(runningWorkout.time) seconds. Your candence was \(runningWorkout.cadence) steps per minute.")
 }
 func describeSwim(swimmingWorkout: Swim){
-    print("You swam \(swimmingWorkout.distance) metres in \(swimmingWorkout.time) seconds using a \(swimmingWorkout.stroke) stroke.")
+    print("You swam \(swimmingWorkout.distance) meters in \(swimmingWorkout.time) seconds using a \(swimmingWorkout.stroke) stroke.")
 }
     
 
